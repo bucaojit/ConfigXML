cf67df8154297a7805e6fac1beacf12d60337511
Oliver <obucaojit@gmail.com>
2017-09-06 14:05:01 -0700

    Push

--- ConfigXML.py
+++ ConfigXML.py
@@ -23,8 +23,54 @@ class xmlparser(object):
       default=option.find('default')
       if label != -1 and default != -1:
         self.defaultDict[label.string]=default.string
-    # for entry in self.configFile.configuration:
+    first = True
+    
+    for entry in self.defaultConfig.fetchText():
+      if first:
+        first = False
+        continue
       
+      currentValue = entry.contents
+      entriesDict = {}
+      # Check if key exists in the dictionary
+      if len(entry.contents) > 1:
+        # Process as a separate dictionary
+        for singleEntry in entry.contents:
+          if singleEntry == '\n':
+            continue
+          entriesDict[singleEntry.name] = singleEntry.contents
+        currentValue = entriesDict
+          
+      if entry.name in self.configDict:
+        existingEntry = self.configDict[entry.name]
+        if not isinstance(existingEntry, list):
+          list(existingEntry).append(currentValue)
+        else:
+          newlist = []
+          newlist.append(existingEntry)
+          newlist.append(currentValue)
+          self.configDict[entry.name] = newlist
+      else:
+        self.configDict[entry.name] = currentValue
+  def getConfigValue(self, configDictionary, query):
+    querylist = query.split(".")
+    if len(querylist) == 1:
+      return configDictionary[querylist]
+    
+    getConfigValue(configDictionary[querylist[0]], ".".join(querylist[1:]))
+    
+  def getConfigValue(self, query):
+    return self.getConfigValue(self.configDict, query)
+      
+          
+        
+    #  value.fetchText()
+    #  Ignore the first, process the rest
+    #  First one seems to be the outer tag
+    #  If there is still one level down then dictionary
+    #  If more exists then create a list
+    #  Leave up to user to know if something is a list
+    #    Have them iterate more
 
   def getValue(self, key):
     """
@@ -43,8 +89,9 @@ class xmlparser(object):
     # - OR return an array of dictionaries, iterate through the array
     # - User will always need to know the structure of their config file
     return "Array of dictionary entries"
-    
+  
 
-
-		
-
+# Notes:
+# Use - 
+# func: find, findAll, fetchText, firstText, getString, getText, 
+# tag:  contents, name
